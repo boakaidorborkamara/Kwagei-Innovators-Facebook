ff76c3774886d84ad51e96d2dc3eba31f7f0b67c
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-09-06 23:33:24 +0000

    style layout

---
+++ script.js
@@ -0,0 +1,34 @@
+let innovators = {
+    Albin:{
+        "first-name": "Albin",
+        "last-name": "Karmoh",
+        "contact": "0886774324",
+        "img-url": "www.imageurl.com"
+    },
+    Ben:{
+        "first-name": "Ben",
+        "last-name": "Ngafaun",
+        "contact": "0886774324",
+        "img-url": "www.imageurl.com"
+    },
+    Boakai:{
+        "first-name": "Boakai",
+        "last-name": "Kamara",
+        "contact": "0886774324",
+        "img-url": "www.imageurl.com"
+    },
+    Joseph:{
+        "first-name": "Joseph",
+        "last-name": "Gbakbutu",
+        "contact": "0886774324",
+        "img-url": "www.imageurl.com"
+    },
+    James:{
+        "first-name": "James",
+        "last-name": "Kumateh",
+        "contact": "0886774324",
+        "img-url": "www.imageurl.com"
+    }
+}
+
+console.log(innovators);
